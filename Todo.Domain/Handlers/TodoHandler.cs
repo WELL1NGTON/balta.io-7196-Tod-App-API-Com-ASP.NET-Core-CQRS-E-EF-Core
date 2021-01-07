d464c9c9c7b128d8c9e8054ccbfe9586774e2db5
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 12:54:19 -0300

    30. Manipulando a atualização da tarefa

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -9,7 +9,8 @@ namespace Todo.Domain.Handlers
 {
     public class TodoHandler :
         Notifiable,
-        IHandler<CreateTodoCommand>
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -34,6 +35,30 @@ namespace Todo.Domain.Handlers
 
             // Salvar no banco
             _repository.Create(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(
+                    false,
+                    "Ops, parece que sua tarefa está errada!",
+                    command.Notifications
+                );
+
+            // Recupera o TodoItem (Rehidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o título
+            todo.UpdateTitle(command.Title);
+
+            // Salvar no banco
+            _repository.Update(todo);
 
             // Retorna o resultado
             return new GenericCommandResult(true, "Tarefa salva", todo);
