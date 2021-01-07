1c0f57385dc27e8a0d9b772345d2e2e092fc7f7d
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 14:48:07 -0300

    47. Testando a API

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Api.Controllers
 {
@@ -8,8 +11,18 @@ namespace Todo.Domain.Api.Controllers
     [Route("v1/todos")]
     public class TodoController : ControllerBase
     {
-        [HttpPost]
         [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAll("andrebaltieri");
+        }
+
+
+        [Route("")]
+        [HttpPost]
         public GenericCommandResult Create(
             [FromBody] CreateTodoCommand command,
             [FromServices] TodoHandler handler
