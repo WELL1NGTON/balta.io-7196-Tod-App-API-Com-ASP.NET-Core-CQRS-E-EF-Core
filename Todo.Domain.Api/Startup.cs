1a69be60b3ac7b4e27ca6b89a50142be272f6fab
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 15:19:24 -0300

    50. Configurando o SQL Server

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -26,8 +26,9 @@ namespace Todo.Domain.Api
 
             services.AddControllers();
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            // services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
