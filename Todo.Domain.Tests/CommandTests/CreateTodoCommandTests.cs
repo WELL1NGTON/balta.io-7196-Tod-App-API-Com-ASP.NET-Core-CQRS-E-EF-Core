4e2edf6e282006db3ecb2deb597541c17ee03ebf
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 11:01:37 -0300

    25. Refatorando os Tests

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -7,22 +7,25 @@ namespace Todo.Domain.Tests.CommandTests
     [TestClass]
     public class CreateTodoCommandTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da Tarefa", "Wellington", DateTime.Now);
+
+        public CreateTodoCommandTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido()
         {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            command.Validate();
-
-            Assert.AreEqual(false, command.Valid);
+            Assert.AreEqual(false, _invalidCommand.Valid);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido()
         {
-            var command = new CreateTodoCommand("Titulo da Tarefa", "Wellington", DateTime.Now);
-            command.Validate();
-
-            Assert.AreEqual(true, command.Valid);
+            Assert.AreEqual(true, _validCommand.Valid);
         }
     }
 }
