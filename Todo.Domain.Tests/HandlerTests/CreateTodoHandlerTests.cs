babf7265bb9959f9c06b3881c7a1372c221b1e12
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 11:24:00 -0300

    28. Testando o Handler

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -9,18 +9,23 @@ namespace Todo.Domain.Tests.HandlerTests
     [TestClass]
     public class CreateTodoHandlerTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da Tarefa", "Wellington", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+        private GenericCommandResult _result = new GenericCommandResult();
+
         [TestMethod]
         public void Dado_um_comando_invalido_deve_interromper_a_excução()
         {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(new FakeTodoRepository());
-            Assert.Fail();
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(false, _result.Success);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido_deve_criar_a_tarefa()
         {
-            Assert.Fail();
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(true, _result.Success);
         }
     }
 }
