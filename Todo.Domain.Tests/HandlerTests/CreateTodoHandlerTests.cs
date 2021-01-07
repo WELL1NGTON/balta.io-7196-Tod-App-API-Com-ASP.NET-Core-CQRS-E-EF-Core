6b01a19a6a19c2403fb3af5f9dfe14d9a1250278
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 11:13:45 -0300

    27. Fake Repositories

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Todo.Domain.Commands;
 using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests
 {
@@ -12,7 +13,7 @@ namespace Todo.Domain.Tests.HandlerTests
         public void Dado_um_comando_invalido_deve_interromper_a_excução()
         {
             var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(null);
+            var handler = new TodoHandler(new FakeTodoRepository());
             Assert.Fail();
         }
 
@@ -23,3 +24,5 @@ namespace Todo.Domain.Tests.HandlerTests
         }
     }
 }
+
+// * Balta: "Se você quiser usar um Mock, eu recomendo o MOQ que é um FrameWork bem legal"
