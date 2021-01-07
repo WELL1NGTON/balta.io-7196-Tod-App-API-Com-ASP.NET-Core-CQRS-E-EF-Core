34f1e6201e78ba674e999669a88c87db0768ec61
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-07 10:04:42 -0300

    11. Design By Contracts

--- Todo.Domain/Commands/CreateTodoCommand.cs
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -1,5 +1,6 @@
 using System;
 using Flunt.Notifications;
+using Flunt.Validations;
 using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
@@ -23,7 +24,12 @@ namespace Todo.Domain.Commands
 
         public void Validate()
         {
-
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                    .HasMinLen(User, 6, "User", "Usuário inválido!")
+            );
         }
     }
 }
